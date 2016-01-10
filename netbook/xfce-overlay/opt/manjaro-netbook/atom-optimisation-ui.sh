08ee5b5f424209f9baa3e981215bfd35dcee11f0
tulliana <mdemiray@msn.com>
2016-01-10 12:07:23 +0200

    old profiles removed

--- netbook/xfce-overlay/opt/manjaro-netbook/atom-optimisation-ui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /opt/manjaro-netbook/
-
-if [ ! -f /bootmnt/.miso ] && \
-   zenity --info --title="Netbook edition configuration" --text="Now we will complete the Netbook edition configuration.\nYou will be asked to enter your password."; then
-
-      gksu ./atom-optimisation-ui-root.sh
-fi
